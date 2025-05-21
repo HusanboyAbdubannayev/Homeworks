86a8d56d1ef150fed1c666785f9dac3b5d890653
HusanboyAbdubannayev <husanboyabdubannayev8@gmail.com>
2025-05-21 14:21:30 +0500

    Variables and operators

---
+++ 1-dars/index.js
@@ -0,0 +1,90 @@
+// console.log("hello js");
+// let a = "hello";
+// var b = "hello js";
+// const c = "js";
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// b = "java";
+// console.log(b);
+// c = "java";
+// console.log(c);
+// console.log(a);
+// console.log(b);
+// var a = "js1";
+//
+// console.log(a);
+//
+// let b = "js2";
+//
+// console.log(b);
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+
+// let num = 4;
+
+// console.log(num);
+
+// let num = 0;
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+// let num = 0;
+// console.log(90071992547409921n + 2n);
+
+// let num = 4;
+// let str = "hello js";
+// console.log(num);
+// console.log(str);
+
+// let str = "hello js 'lorem'";
+// let str1 = 'hello js "lorem"';
+// let str2 = `hello js 'lorem"`;
+// console.log(str);
+// console.log(str1);
+// console.log(str2);
+
+// x = "salom";
+// let str = "hello js 'lorem'";
+// let str1 = 'hello js "lorem"';
+// let str2 = `hello js ${x} 'lorem"`;
+// console.log(str);
+// console.log(str1);
+// console.log(str2);
+
+// let a = true;
+// let b = false;
+// console.log(a);
+// console.log(b);
+
+// console.log(3 == 4);
+
+// let Null = null;
+// console.log(Null);
+
+// let syb = "a";
+// console.log(Symbol("id"));
+
+// let fruits = ["olma", "banan", "anor"];
+// console.log(fruits);
+
+// let user = {
+//   name: "Ali",
+//   age: 25,
+// };
+
+// console.log(user);
+
+// function greet() {
+//   return "salom";
+// }
+// greet();
+// console.log(greet);
+
+// console.log(alert("hello"));
+// console.log(promt("your name"));
+
+// let a = 10;
+// var b = "de";
