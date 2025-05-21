9d1d102f46daeb4d87d8bae3b50c7c2bf44624b2
HusanboyAbdubannayev <husanboyabdubannayev8@gmail.com>
2025-05-21 14:24:23 +0500

    If , else if , else, ternary

---
+++ 4-dars/index.js
@@ -0,0 +1,109 @@
+// syntax if(shart){code}
+// if (1 == false) {
+//   console.log("hello");
+// }
+// if (true) {
+//   console.log("hi");
+// }
+// let yosh = 7;
+// if (yosh >= 10) {
+//   console.log("siz hal yoshsiz");
+// }
+// if (1 == 0) {
+//   console.log("hello");
+// } else {
+//   console.log("bye");
+// }
+// let baho = 1;
+// if (baho >= 5) {
+//   console.log("Alo");
+// } else if (baho >= 4) {
+//   console.log("Qoniqarli");
+// } else if (baho >= 3) {
+//   console.log("Yahshi");
+// } else {
+//   console.log("Yomon");
+// // }
+// num = 20;
+// if (1 == 0) {
+//   console.log("hi");
+// } else {
+//   if (num >= 18) {
+//     console.log("ichkaridagi if");
+//   } else {
+//     if (0 == 0) {
+//       console.log("boom");
+//     }
+//     console.log("else ichidagi else");
+//   }
+//   console.log("bye");
+// }
+// if (0 == 1) console.log("hello");
+// else console.log("hi");
+
+// let title = "Webbrain Academy";
+// if ((title = "Webbrain Academy")) console.log(true);
+// else console.log(false);
+
+// if ((title = "0")) console.log(true);
+// else console.log(false);
+
+// let title = "0"
+// if (title) console.log(true);
+// else console.log(false);
+
+// let title = "Webbrain academy";
+// if (title == "Webbrain Academy") console.log(true);
+// else console.log(false);
+
+// syntax conditional ? code : code
+
+// false ? console.log("hello") : console.log("hi");
+
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// false ? console.log("hello") : console.log("hi");
+// if (1 == 1) {
+//   console.log("hello");
+// }
+// else{
+//     console.log("hi");
+// }
+
+// mehmon = "ayol";
+// if (mehmon == "erkak") {
+//   console.log("ongdagi zalga");
+// } else {
+//   console.log("chapdagi zalga");
+// }
+
+// parol = 10;
+// if (parol > 5) {
+//   console.log("togri");
+// } else {
+//   console.log("natogri");
+// }
+
+// yosh = 2;
+// if (yosh > 18) {
+//   console.log("kirish munkin");
+// } else {
+//   console.log("kirish munkin emas");
+// }
+
+// cola = "suvcola";
+// if (cola == "cola") {
+//   console.log("cola zararli");
+// } else if (cola == "suv") {
+//   console.log("suv zararsiz");
+// } else {
+//   console.log("suv ichmang");
+// }
---
+++ 4-dars/sdw.md
@@ -0,0 +1,29 @@
+                                                       If, Else, Else If
+
+    If --> bu shart operatori bolib ifda agar berilgan shart rost bolsa yani truega teng bolsa ishlaydi hozir unga syntax tuzamn
+       --> syntax // if (shart yoziladi) {code}
+            voyaga yetganini chiqarish 
+           ----------------------------------------------------------------------------------------------------------------------------------------------------
+       --> 
+       erkak yo ayolni ongdagi yo chapdagi zalga kirishini qilish
+           ----------------------------------------------------------------------------------------------------------------------------------------------------
+    else --> bu esa agar ifda berilgan shart falsega teng bolib ishlamay qolsa unda elseda berilgan shart ishlaydi.Ahag ifdagi shart
+             rost bosla yani truega teng bosla elsedagi qiymat ishlamaydi
+         --> syntax // else{code}
+           -------------------------------------
+          agar yoshi 18yoshdan katta bolsa kirish munkin 18yoshdan kichkina bolsa kirish munkin emas degan soz chiqishi kerak
+           -------------------------------------
+           parol togri yo natogriligini chiqarish
+           ----------------------------------------------------------------------------------------------------------------------------------------------------
+    else if --> else if esa bir nechta shart bajarsa oshanda ishlatiladi.har bir if alohida guruh deb hisoblanadi else if kop marta
+                yozilsa birinchi true chiqqan shart bajariladi kyn toxtaydi pastdagi shartlarni bajarmaydi
+         --> syntax // else if {}
+        endi baho yahshi qoniqarli alo yoki yomon chiqarishini qilamiz
+  
+                                                                          Ternary operator
+                                                                          
+     Ternary operator --> bu if da tuzilgan shartlarni qisqartirish uchun ishlatiladi.
+     
+     syntax --> // (shart) ? code : code 
+     	
+     yoshi 18dan kichkina lekin yoshi 7dan katta boladigan bolsa  
