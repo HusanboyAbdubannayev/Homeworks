45b3808522122544a0340e154935ed5b705299f7
HusanboyAbdubannayev <husanboyabdubannayev8@gmail.com>
2025-05-21 14:25:07 +0500

    switch , for , while , do while,

---
+++ 5-dars/index.js
@@ -0,0 +1,106 @@
+// let a = "quyosh";
+// if (a == "yomgir") {
+//   console.log("bugun yomgir");
+// }
+// switch (a) {
+//   case "yomgir":
+//     console.log("bugun yomgir yogishi munkin");
+//     console.log("bugun yomgir yogishi munkin");
+//   case "qor":
+//     console.log("bugun qor yogmaydi");
+//   default:
+//     console.log("bugun yomgir ham qor ham yoq");
+// }
+// console.log("abdurahm");
+// console.log("ilos");
+// console.log("sulton");
+// console.log("avaz");
+// console.log("muhammadsodiq");
+// console.log("jasur");
+// console.log("abdurahm");
+// console.log("ilos");
+// console.log("sulton");
+// console.log("avaz");
+// console.log("muhammadsodiq");
+// console.log("jasur");
+// console.log("abdurahm");
+// console.log("ilos");
+// console.log("sulton");
+// console.log("avaz");
+// console.log("muhammadsodiq");
+// console.log("jasur");
+// console.log("abdurahm");
+// console.log("ilos");
+// console.log("sulton");
+// console.log("avaz");
+// console.log("muhammadsodiq");
+// console.log("jasur");
+// console.log("abdurahm");
+// console.log("ilos");
+// console.log("sulton");
+// console.log("avaz");
+// console.log("muhammadsodiq");
+// console.log("jasur");
+// console.log("abdurahm");
+// console.log("ilos");
+// console.log("sulton");
+// console.log("avaz");
+// console.log("muhammadsodiq");
+// console.log("jasur");
+// let x = 1;
+// while (x <= 10) {
+//   console.log(x);
+//   xlet x = 1;
+// while (x <= 10) {
+//   console.log(x);
+//   x = x + 1;
+// } = x + 1;
+// }
+
+// let i = 1;
+// let sum = 0;
+// while (i <= 100) {
+//   if (i % 2 === 0) {
+//     sum = sum + i;
+//   }
+//   i++;
+// }
+
+// console.log(sum);
+
+// while (condition) {
+//   code;
+// }
+// let parol;
+
+// do {
+//   parol = prompt("Parol kiriting");
+// } while (parol !== "102");
+// console.log("Parol togri");
+
+// for (boshlanishi,manzil,step){
+//   code;
+// }
+
+// for (let x = 1; x <= 10; x++) {
+//   console.log(x);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log("--------------------");
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(${i} * ${j} = ${i * j});
+
+//   }
+// }
+
+// let i = 1;
+// let sum = 0;
+// while (i <= 10) {
+//   if (i % 2 === 0) {
+//     sum = sum + i;
+//   }
+//   continue;
+//   i++;
+// }
+// console.log(sum);
---
+++ 5-dars/node.md
@@ -0,0 +1,57 @@
+# Switch
+
+1.Switch ifga ohshash
+
+# misol{
+
+}
+
+# Synatx {
+
+    switch (key) {
+    case value:
+      break;
+      default:
+      break;
+    }
+
+}
+
+# For loops
+
+1.For loop 3 turga bolinadi (While,do while, for loop)
+
+# while
+
+# Syntax{
+
+while (condition) {
+...
+}
+
+}
+
+# Do while
+
+# Syntax{
+
+    do {
+
+...
+
+} while (condition);
+}
+
+# For
+
+# Syntax{
+
+    for (let x = 0; x <= 10; x++) {
+
+    }
+
+}
+
+# Contineu
+
+1.bu stop qilish uchun ishlatiladi tepasidan code oqib keladi qachon contineu korguncha korgandan kyn pastdagilarini oqimadi.
