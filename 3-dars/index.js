9595bf2e5f013d61763ab7019ba925f515542fe7
HusanboyAbdubannayev <husanboyabdubannayev8@gmail.com>
2025-05-21 14:23:45 +0500

    Type conversion

---
+++ 3-dars/index.js
@@ -0,0 +1,44 @@
+// let a = 4;
+// let b = "4";
+// console.log(a + b);
+
+// let a = 4;
+// let b = typeof Number("4");
+// console.log(a + b);
+
+// let num = Number("12");
+// let nan = Number("hi");
+
+// let num = 100;
+// let str = num.toString();
+// console.log(num);
+// console.log(str);
+
+// false ==> 0, "",'',``, false , null , undefined
+// true shu tepadagilardan boshqa hammasi
+
+// let bool = true;
+// let num = Number(bool);
+// console.log(bool);
+// console.log(num);
+
+// let result = 0/0 //NaN
+// // NaN(Not a number)
+
+// let sum = 10 + 5; // 15
+// console.log(sum);
+
+// let diff = 10 - 5; // 5
+// console.log(diff);
+
+// let product = 10 * 5; // 50
+// console.log(product);
+
+// let quetient = 10 / 2; // 5
+// console.log(quetient);
+
+// let remainder = 10 % 3; // 1
+// console.log(remainder);
+
+// let power = 2 ** 3; // 8
+// console.log(power);
